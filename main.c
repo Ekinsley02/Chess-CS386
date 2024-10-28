ace75d711c876f4903f087f99f2791c6a7731e11
Ekinsley02 <144740384+Ekinsley02@users.noreply.github.com>
2024-10-27 18:06:53 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -20,6 +20,33 @@ void outputBoard(ChessBoardType **board)
       printf("\n");
       fflush(stdout);
       }
+   
+   // Output sides (Player or Opponent)
+   for( rowIndex = 0; rowIndex < BOARD_SIZE; rowIndex++ )
+      {
+      for (colIndex = 0; colIndex < BOARD_SIZE; colIndex++)
+         {
+            
+         printf("%c ", board[rowIndex][colIndex].side);
+         }
+         
+      printf("\n");
+      fflush(stdout);
+      }
+      
+   // Output highlights (1 or 0 dependining on highlight)
+   for( rowIndex = 0; rowIndex < BOARD_SIZE; rowIndex++ )
+      {
+         
+      for( colIndex = 0; colIndex < BOARD_SIZE; colIndex++ )
+         {
+            
+         printf("%d ", board[rowIndex][colIndex].highlight);
+         }
+         
+      printf("\n");
+      fflush(stdout);
+      }
    }
 
 int main()
@@ -27,6 +54,14 @@ int main()
    
    // initialize functions/variables
       ChessBoardType **board;
+      
+      char currentType, currentTurn;
+      
+      bool initialPawn = false, gameRunning = true;
+      
+      int start_row = 0, start_col = 0;
+      
+      int currentState = SELECTING;
       
    // create game board array
    board = initializeChessBoard( );
@@ -39,6 +74,37 @@ int main()
    
    // Output initial board
    outputBoard( board );
+   
+   currentTurn = 'P';
+   
+   while( gameRunning )
+      {
+      
+      if( scanf( "%d %d", &start_row, &start_col ) != 2 ) 
+         {
+            
+         printf("Invalid input, exiting loop.\n");
+         fflush(stdout);
+         break;
+         }
+         
+      currentType = board[start_row][start_col].type;
+      
+      // Process the move if valid for highlighting
+      if( checkIfValidPosition( board, currentType, currentTurn, start_row, start_col, start_row, start_col, &currentState, initialPawn ) )
+         {
+               
+         // highlight potential attack points using HIGHLIGHT flag
+         printf("Highlighting potential moves.\n");
+         fflush(stdout);
+         highlightAttack( board, start_row, start_col, currentType, currentTurn, HIGHLIGHT, currentState, initialPawn );
+            
+         printf("Outputting board with highlights.\n");
+         fflush(stdout);
+         }
+         
+      outputBoard(board);
+      }
    
    return(0);
    }
