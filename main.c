a91607d5395f9106725a7aca33b9ffbf6875c14c
Ekinsley02 <144740384+Ekinsley02@users.noreply.github.com>
2024-11-17 17:48:13 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -47,7 +47,7 @@ void outputBoard(ChessBoardType **board, int gameCondition)
       printf("\n");
       fflush(stdout);
       }
-   
+   printf("%d\n", gameCondition);
    }
 
 int main()
@@ -86,27 +86,12 @@ int main()
       initialPawn = false;
       inCheck = false;
       
-      if( isInCheck( board, currentTurn, INCHECK ) && !isCheckmate( board, currentTurn, NONE ) )
-         {  
-         
-         gameCondition = 1;
-         }
-      
-      
-      else if( isStalemate( board, currentTurn ) )
-         {
-        
-         gameCondition = 2;
-         }
-      
       if( gameCondition == 1 || gameCondition == 2 )
          {
          
          gameRunning = false;
          outputBoard( board, gameCondition );
          }
-      
-       printf("%d\n", gameCondition);
 
       scanf( "%d %d", &start_row, &start_col );
          
@@ -159,7 +144,19 @@ int main()
             movePiece(board, currentTurn, end_row, end_col, currentState, start_row, start_col);
             
             currentTurn = switchTurn( currentTurn );  
-            
+
+            if( !isCheckmate( board, currentTurn, NONE ) )
+               {  
+         
+               gameCondition = 1;
+               }
+      
+      
+            else if( isStalemate( board, currentTurn ) )
+               {
+        
+               gameCondition = 2;
+               }
             }
          }
       
